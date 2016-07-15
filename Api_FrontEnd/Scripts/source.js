53b3fd0870a8b04593b921462768a7ee47cbcb02
Preston (desktop) <smith.preston2@gmail.com>
2016-07-15 13:01:45 -0600

    Animate intro section

---
+++ Api_FrontEnd/Scripts/source.js
@@ -0,0 +1,15 @@
+﻿function runPageFunctions() {
+    $(document).ready(onStart);
+}
+
+function onStart() {
+    $("#start-arrow").click(function () {
+        $("#github-search")
+            .animate({
+                marginTop: 0
+            }, 500);
+    });
+}
+
+runPageFunctions();
+
