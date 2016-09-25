0092376e128e3a8013aea23cda83acafb20503b1
Preston (desktop) <smith.preston2@gmail.com>
2016-09-25 10:12:40 -0600

    Stop infinite loading - page clear

--- Api_FrontEnd/Scripts/main.js
+++ Api_FrontEnd/Scripts/main.js
@@ -1,34 +1,12 @@
-requirejs(["jquery", "GITAPP"],
-    (function ($, GITAPP) {
+requirejs(["jquery", "GITAPP"], function($, GITAPP) {
 
-        startUp();
+    console.log($);
 
-        function startUp() {
-            $(document).ready(initApp());
-        }
-
-        function initApp() {
-            var self = this;
-
-            this.searchButton = document.getElementById("search-button");
-            this.arrow = document.getElementById("start-arrow");
-            this.organization = {};
-
-            searchButton.click(function() {
-                var orgName = document.getElementById("organization-search-box").textContent;
-                self.organization = new GITAPP.Organization(orgName);
-            });
-
-            arrow.click(function () {
-                $("#github-search")
-                    .animate({
-                        marginTop: 0
-                    }, 500);
-            });
-        };
-
-        
-
-    }));
+    $("#start-arrow").click(function() {
+        $("#github-search").animate({
+                    marginTop: 0
+                },500);
+    });
+});
 
 
