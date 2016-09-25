6b9fe9abdff1f6425221e7a6749110fb61ce0a3f
Preston (desktop) <smith.preston2@gmail.com>
2016-09-25 11:49:32 -0600

    Attach search function to button

--- Api_FrontEnd/Scripts/main.js
+++ Api_FrontEnd/Scripts/main.js
@@ -1,13 +1,24 @@
 requirejs(["jquery", "GITAPP"], function($) {
 
-    console.log($);
-    console.log(GITAPP);
+    var self = this;
+    this.organization = {};
+    
+    $("#search-button").click(function (event) {
+        event.preventDefault();
+        var orgName = $("#organization-search-box")[0].value;
+
+        console.log(orgName);
+        self.organization = new GITAPP.Organization(orgName);
+        
+        });
 
     $("#start-arrow").click(function() {
         $("#github-search").animate({
                     marginTop: 0
                 },500);
     });
+
+    
 });
 
 
