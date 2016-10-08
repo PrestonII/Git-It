43bb86e905d11ccdacff156b835ad09e2f5c1fb2
Preston Smith <PrestonII@users.noreply.github.com>
2016-10-08 12:37:47 -0600

    Fix paths per gh-pages standards (#3)
    * Move all to main directory
    
    * Fix paths per github-pages standards
    
    http://stackoverflow.com/questions/39920249/github-pages-site-cant-find-content

--- Scripts/init.js
+++ Scripts/init.js
@@ -1,12 +1,12 @@
 requirejs.config({
     baseUrl: "../Scripts",
     paths: {
-        angular:            "_lib/angular",
-        ngRoute:            "_lib/angular-route",
-        ngResource:         "_lib/angular-resource",
+        angular:            "all_lib/angular",
+        ngRoute:            "all_lib/angular-route",
+        ngResource:         "all_lib/angular-resource",
 
-        jquery:             "_lib/jquery-1.10.2.min",
-        bootstrap:          "_lib/bootstrap.min",
+        jquery:             "all_lib/jquery-1.10.2.min",
+        bootstrap:          "all_lib/bootstrap.min",
         
         Organization:       "app/objs/Organization",
         Repository:         "app/objs/Repository",
